29ed6f499a61a157ca116d8cc9f912d257535a6a
Pasit Jongngamwilai <cchokun76@gmail.com>
2025-06-18 10:40:49 +0700

    add.center

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main>
+      <center>
       <h1>Mr. Pasit Jongngamwilai</h1>
-      <h2>No. 022 ปวส. ตรง</h2>
+      <h2>No. 022 ปวส.</h2>
+      </center>
     </main>
   );
 }
