78e32615bcf309202b74e83bc33eb03023478ac9
Pasit Jongngamwilai <cchokun76@gmail.com>
2025-07-02 10:53:10 +0700

    Add-Card-1row3col

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import Carousel from "@/components/Carousel";
+import Card from "@/components/Card";
 export default function Home() {
   return (
     <main className="inset-0 flex items-center justify-center bg-gray-900 ">
       <Carousel/>
+      <Card/>
       <h1 className="pt-28 text-6xl font-bold text-blue-200 text-center">
         Mr. Pasit Jongngamwilai
       </h1>
---
+++ components/Card.js
@@ -0,0 +1,53 @@
+export default function Card() {
+  return (
+    <main className="container my-4">
+        
+        <div className="row">
+            <h2 className="pt-28 text-6xl font-bold text-center g-2">
+                รวมรูปเด็กโหด 3 คน
+            </h2>
+        </div>
+
+        <div className="row row-cols-1 row-cols-md-3 g-4 mt-1">
+            <div className="col">
+            <div className="card">
+                <img
+                src="/image/Card/babyLeo.png"
+                className="card-img-top"
+                alt="..."
+                />
+                <div className="card-body">
+                <p className="card-text">รูปของเด็กโหด.</p>
+                </div>
+            </div>
+            </div>
+
+            <div className="col">
+            <div className="card">
+                <img
+                src="/image/Card/babyLeo.png"
+                className="card-img-top"
+                alt="..."
+                />
+                <div className="card-body">
+                <p className="card-text">รูปของเด็กโหด.</p>
+                </div>
+            </div>
+            </div>
+
+            <div className="col">
+            <div className="card">
+                <img
+                src="/image/Card/babyLeo.png"
+                className="card-img-top"
+                alt="..."
+                />
+                <div className="card-body">
+                <p className="card-text">รูปของเด็กโหด.</p>
+                </div>
+            </div>
+            </div>
+        </div>
+    </main>
+  );
+}
