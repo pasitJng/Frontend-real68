9dd139c9915aa1fc45e8c14bc9284e19ec474cb6
Pasit Jongngamwilai <cchokun76@gmail.com>
2025-07-23 13:08:31 +0700

    move-alert to layout

--- app/layout.js
+++ app/layout.js
@@ -1,40 +1,37 @@
-//import { Geist, Geist_Mono } from "next/font/google";
-//import "./globals.css";
+'use client';
+
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Prompt } from "next/font/google";
+
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
+import AlertModal from "@/components/AlertModal";
 
-//const geistSans = Geist({
-//  variable: "--font-geist-sans",
-//  subsets: ["latin"],
-//});
-
-//const geistMono = Geist_Mono({
-//  variable: "--font-geist-mono",
-//  subsets: ["latin"],
-//});
 const prompt = Prompt({
   subsets: ["thai", "latin"],
   weight: ["400", "500", "700"],
   display: "swap",
 });
 
-export const metadata = {
-  title: "WebSite-Pasit022",
-  description: "อะไร",
-};
-
 export default function RootLayout({ children }) {
+    const [alertVisible, setAlertVisible] = useState(false); //*ปิดใช้งาน Alert ให้พิมพ์ false*
+  
+    const handleClose = () => {
+      setAlertVisible(false);
+    };
+
   return (
     <html lang="en">
       <body className={`d-flex flex-column min-vh-100 ${prompt.className}`}>
-  
         <Navigation />
-        <main className="flex-fill">
-          {children}
-        </main>
+
+        {/* ✅ AlertModal จะแสดงในทุกหน้า */}
+        <AlertModal visible={alertVisible} onClose={handleClose} />
+
+        <main className="flex-fill">{children}</main>
+
         <Footer />
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -5,18 +5,12 @@ import Link from "next/link";
 import Carousel from "@/components/Carousel";
 import Card from "@/components/Card";
  import BannerNotice from "@/components/BannerNotice";
-import AlertModal from "@/components/AlertModal"; // เปลี่ยนชื่อให้ตรงกัน
 
 export default function Home() {
-  const [alertVisible, setAlertVisible] = useState(false); //*ปิดใช้งาน Alert ให้พิมพ์ false*
 
-  const handleClose = () => {
-    setAlertVisible(false);
-  };
   
   return (
     <div>
-      <AlertModal visible={alertVisible} onClose={handleClose} />
       <main className="inset-0 flex items-center justify-center bg-gray-900">
         <Carousel />
         <Card />
